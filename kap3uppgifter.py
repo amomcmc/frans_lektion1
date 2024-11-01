ad1d4e6451acc562340fa3eee99a35650c0ea787
amomcmc <diyar99@live.se>
2024-11-01 15:18:41 +0100

    kap3uppgift1.py

--- kap3uppgifter.py
+++ kap3uppgifter.py
@@ -7,18 +7,4 @@ for siffra in range(1, 101):
     elif siffra % 5 == 0:
         print("Buzz")
     else: 
-        print(siffra)
-        
-        
-# for i in range(1, 101):    # Loopa från 1 till 100
-#     if i % 3 == 0 and i % 5 == 0:    # Om talet är delbart med både 3 och 5
-#         print("FizzBuzz")
-#     elif i % 3 == 0:    # Om talet är delbart med 3
-#         print("Fizz")
-#     elif i % 5 == 0:    # Om talet är delbart med 5
-#         print("Buzz")
-#     else:
-#         print(i)    # Om inget av ovanstående gäller, skriv ut talet självt
-
-    
-        
+        print(siffra) 
